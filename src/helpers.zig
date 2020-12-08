cab4b324cd3479fd23fb39f7616f223fc405af56
CodeMyst <code@myst.rs>
2020-12-08 18:22:03 +0100

    fixed helpers functions

--- src/helpers.zig
+++ src/helpers.zig
@@ -1,11 +1,11 @@
 const math = @import("std").math;
 
 /// converts degrees to radians
-pub inline fn rad(deg: f32) f32 {
-    return deg * (math.pi / 180);
+pub inline fn rad(d: f32) f32 {
+    return d * (math.pi / 180);
 }
 
 /// converts radians to degrees
-pub inline fn deg(rad: f32) f32 {
-    return rad * (180 / math.pi);
+pub inline fn deg(r: f32) f32 {
+    return r * (180 / math.pi);
 }
